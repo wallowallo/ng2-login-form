7b4c2178b427d351c9c033f3bf6037aaffa204f7
Krister <krister.torsvik@gmail.com>
2016-12-15 11:48:04 +0100

    sign up pageworking, need log in route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import {
+    FormBuilder,
+      FormControl,
+        FormGroup,
+} from '@angular/forms';
+
+const USERS: registeredUser[] = [
+ {firstName: 'test', lastName: 'test', email: 'test', password: 'test'}
+];
+
+export class registeredUser {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
 
 @Component({
+  moduleId: 'module.id',
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class SignUpComponent {
-  submitted = false;
-  onSubmit() { this.submitted = true;
-  console.log(this.submitted);
+  users = USERS;
+
+  signUp: FormGroup;
+  firstName: FormControl;
+  lastName: FormControl;
+  email: FormControl;
+  password: FormControl;
+
+constructor (builder: FormBuilder) {
+  this.firstName = new FormControl('', []);
+  this.lastName = new FormControl('', []);
+  this.email = new FormControl('', []);
+  this.password = new FormControl('', []);
+  this.signUp = builder.group({
+    firstName: this.firstName,
+    lastName: this.lastName,
+    email: this.email,
+    password: this.password
+  });
+}
+
+  newUser() { 
+    this.users.push(this.signUp.value);
+    this.signUp.reset();
+    console.log(this.users);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { SignUpComponent } from './app.component';
@@ -11,6 +11,7 @@ import { SignUpComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     FormsModule,
     HttpModule
   ],
